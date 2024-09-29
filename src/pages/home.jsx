7100224eabe3a59691e7562d72d9db60dfb1db08
Gabriel Thomas <gabriel.getdev@gmail.com>
2024-09-29 19:28:29 -0300

    Feat: conditional render on forms

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,20 +5,30 @@ import { FormProfessionalExperiences } from "../forms/professional-experiences";
 import { FormScholarshipDetails } from "../forms/scholarship-details";
 import { ProfessionalExperiencesList } from "../forms/professional-experiences-list";
 import { ScholarshipList } from "../forms/scholarship-list";
+import { useContext } from "react";
+import { FormContext } from "../contexts/form-context";
 
 export const Home = () => {
-  return (
-    <Container size="lg">
-      <ExperimentalFormAlert />
-      <FormPersonalData />
-      <>
-        <FormProfessionalExperiences />
-        <ProfessionalExperiencesList experiences={[]} />
-      </>
-      <>
-        <FormScholarshipDetails />
-        <ScholarshipList scholarships={[]} />
-      </>
-    </Container>
-  );
+	const { formStep } = useContext(FormContext);
+
+	return (
+		<Container size="lg">
+			<ExperimentalFormAlert />
+			{formStep === 1 ? (
+				<FormPersonalData />
+			) : formStep === 2 ? (
+				<>
+					<FormProfessionalExperiences />
+					<ProfessionalExperiencesList experiences={[]} />
+				</>
+			) : (
+				formStep === 3 && (
+					<>
+						<FormScholarshipDetails />
+						<ScholarshipList scholarships={[]} />
+					</>
+				)
+			)}
+		</Container>
+	);
 };
